027c7f59da48edc603e18a98cac540ae0258ee51
Nathaniel Miller <dukeoflaser@gmail.com>
2017-04-24 17:54:05 -0700

    Removed index.html view and replace with redirect to sign in page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,11 @@ class HomeController < ApplicationController
   before_action :check_signed_in, only: :index
 
   def check_signed_in
-    redirect_to dashboard_path if signed_in?
+    if signed_in?
+      redirect_to dashboard_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def index
