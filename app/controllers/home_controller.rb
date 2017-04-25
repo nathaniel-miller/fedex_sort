f637acfdda0bc2f236c638e8e5afcb36b195935f
Nathaniel Miller <dukeoflaser@gmail.com>
2017-04-24 17:39:37 -0700

    Add dashboard. Current state - if user is logged in they will be directed to the dashboard. Logout redirects to home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+  before_action :check_signed_in, only: :index
+
+  def check_signed_in
+    redirect_to dashboard_path if signed_in?
+  end
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
-  namespace :user do
-    root :to => "home#dashboard"
-  end
+
+  get '/dashboard', to: "home#dashboard", as: 'dashboard'
 
 end
