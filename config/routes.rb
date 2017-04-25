2e8df856246be29afabd6c01598d289fba5f02e5
Nathaniel Miller <dukeoflaser@gmail.com>
2017-04-24 17:01:02 -0700

    Add dashboard controller, view, route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
+  namespace :user do
+    root :to => "home#dashboard"
+  end
 
 end
