a276d21f41514428c74c60260094a25280790659
Nathaniel Miller <dukeoflaser@gmail.com>
2017-05-02 17:07:27 -0700

    Replace jobs with positions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -9,6 +10,6 @@ Rails.application.routes.draw do
 
   resources :schedules
   resources :team_members
-  resources :jobs
+  resources :positions
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424232324) do
+ActiveRecord::Schema.define(version: 20170503000438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "jobs", force: :cascade do |t|
+  create_table "positions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
