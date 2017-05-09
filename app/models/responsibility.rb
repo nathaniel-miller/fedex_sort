fd996cf069dc0ab6687f25844c1aaf8a1fb637e5
Nathaniel Miller <dukeoflaser@gmail.com>
2017-05-08 20:43:35 -0700

    Rollback TeamMember/Responsibilities/Position associations

--- app/models/responsibility.rb
+++ app/models/responsibility.rb
@@ -2,4 +2,6 @@ class Responsibility < ApplicationRecord
   belongs_to :schedule
   has_and_belongs_to_many :team_members
   has_and_belongs_to_many :positions
+  # belongs_to :team_member
+  # belongs_to :position
 end
