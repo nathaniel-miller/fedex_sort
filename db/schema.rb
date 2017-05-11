6a29998e6dfae55651b933ef864b7150b488c992
Nathaniel Miller <dukeoflaser@gmail.com>
2017-05-10 18:19:05 -0700

    Add user_id column to schedules table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510214236) do
+ActiveRecord::Schema.define(version: 20170511011827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20170510214236) do
     t.datetime "updated_at", null: false
     t.date     "start_date"
     t.date     "end_date"
+    t.integer  "user_id"
   end
 
   create_table "setups", force: :cascade do |t|
