aa93f5a5c4ec84e1b86a67d1b210f6baa505d467
Nathaniel Miller <dukeoflaser@gmail.com>
2017-05-11 20:26:47 -0700

    Tidy up schedule controller

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,42 +1,33 @@
 class SchedulesController < ApplicationController
   before_action :set_schedule, only: [:show, :edit, :update, :destroy]
+  before_filter :common_content, :only => [:new, :edit, :create]
 
-  # GET /schedules
-  # GET /schedules.json
+  def common_content
+    @user = current_user
+    @sort_types = SortType.all
+  end
+
   def index
     @schedules = Schedule.all
   end
 
-  # GET /schedules/1
-  # GET /schedules/1.json
   def show
   end
 
-  # GET /schedules/new
   def new
-    @user = current_user
-    @sort_types = SortType.all
     @schedule = Schedule.new
   end
 
-  # GET /schedules/1/edit
   def edit
-    @user = current_user
-    @sort_types = SortType.all
   end
 
-  # POST /schedules
-  # POST /schedules.json
   def create
-    @user = current_user
-    @sort_types = SortType.all
     @schedule = Schedule.new(schedule_params)
-    # @schedule.generate_responsibilites if @schedule.valid?
 
     respond_to do |format|
       if @schedule.save
         @schedule.update(sort_params)
-        
+
         format.html { redirect_to @schedule, notice: 'Schedule was successfully created.' }
         format.json { render :show, status: :created, location: @schedule }
       else
@@ -46,8 +37,6 @@ class SchedulesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /schedules/1
-  # PATCH/PUT /schedules/1.json
   def update
     respond_to do |format|
       if @schedule.update(schedule_params)
@@ -60,8 +49,6 @@ class SchedulesController < ApplicationController
     end
   end
 
-  # DELETE /schedules/1
-  # DELETE /schedules/1.json
   def destroy
     @schedule.destroy
     respond_to do |format|
@@ -71,32 +58,31 @@ class SchedulesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_schedule
-      @schedule = Schedule.find(params[:id])
-    end
+  def set_schedule
+    @schedule = Schedule.find(params[:id])
+  end
 
-    def schedule_params
-      params.require(:schedule).permit(
-        :start_date,
-        :end_date,
-        :user_id
-      )
-    end
+  def schedule_params
+    params.require(:schedule).permit(
+      :start_date,
+      :end_date,
+      :user_id
+    )
+  end
 
-    def sort_params
-      sort = params[:schedule][:sorts_attributes]["0"]
-      schedule = params[:schedule]
+  def sort_params
+    sort = params[:schedule][:sorts_attributes]["0"]
+    schedule = params[:schedule]
 
-      sort["start_date(1i)"] = schedule["start_date(1i)"]
-      sort["start_date(2i)"] = schedule["start_date(2i)"]
-      sort["start_date(3i)"] = schedule["start_date(3i)"]
-      sort["end_date(1i)"]   = schedule["end_date(1i)"]
-      sort["end_date(2i)"]   = schedule["end_date(2i)"]
-      sort["end_date(3i)"]   = schedule["end_date(3i)"]
+    sort["start_date(1i)"] = schedule["start_date(1i)"]
+    sort["start_date(2i)"] = schedule["start_date(2i)"]
+    sort["start_date(3i)"] = schedule["start_date(3i)"]
+    sort["end_date(1i)"]   = schedule["end_date(1i)"]
+    sort["end_date(2i)"]   = schedule["end_date(2i)"]
+    sort["end_date(3i)"]   = schedule["end_date(3i)"]
 
-      params.require(:schedule).permit(
-        :sorts_attributes => [:start_date, :end_date, :sort_type_ids => []]
-      )
-    end
+    params.require(:schedule).permit(
+      :sorts_attributes => [:start_date, :end_date, :sort_type_ids => []]
+    )
+  end
 end
