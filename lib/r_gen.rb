6736b91c36f7eb3e866870fa62c899ebb638c0d9
Nathaniel Miller <dukeoflaser@gmail.com>
2017-05-12 12:10:26 -0700

    Sketch in thoughts for how algorithm should operate.

--- lib/r_gen.rb
+++ lib/r_gen.rb
@@ -1,15 +1,33 @@
 module RGen
-  def generate_responsibilites
+  def generate_responsibilities
 
+    generate_positions
+    generate_team_members
+
+
+  end
+
+  def generate_positions
     sort_type.setup.positions.each do |p|
       r = Responsibility.create
       r.position = p
       responsibilities << r
     end
-
-    responsibilities.each_with_index do |r, i|
-      r.team_member = TeamMember.all[i]
-    end
-
   end
+
+  def generate_team_members
+    responsibilities.each_with_index do |r, i|
+      r.team_member = sort_type.roster.team_members[i]
+    end
+  end
+
+  #Team Members from Roster only.
+  #Check for vactions
+  #Check for absents -> need something to set single dates here
+  #Check for fixed positions
+  #Check for light duty
+  #Check for newbie
+
+  # Will need sub groups - ex. list of newbie-friendly positions and new employees
+
 end
