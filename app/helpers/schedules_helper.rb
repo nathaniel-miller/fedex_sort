143bfcae166074890b39dec07ca28caf45e27267
Nathaniel Miller <dukeoflaser@gmail.com>
2017-05-15 22:22:30 -0700

    Add get_sort helper for schedule view.

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -2,4 +2,8 @@ module SchedulesHelper
   def columns
     @schedule.dates.count + 1
   end
+
+  def get_sort(sort_type_id, date)
+    @sorts.where(sort_type_id: sort_type_id).find_by(date: date)
+  end
 end
